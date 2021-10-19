09205a5b5fdbec03b0ba3e035dfa0fbb8778de99
kildom <kildom@users.noreply.github.com>
2021-10-19 18:41:43 +0200

    Update ids_gen.sh

--- ids_gen.sh
+++ ids_gen.sh
@@ -4,18 +4,23 @@ set -e
 
 mkdir -p IDS
 
-for i in {0..17}; do 
-	if [ -e IDS/$i.prv ]; then
-		continue
-	fi
+for i in {2..19}; do
+	echo ---- Generating id for $i...
+	echo start daemon
 	sudo zerotier/zerotier-one -d
 	sleep 10
-	sudo pkill -SIGINT zerotier-one
+	echo ctrl-c
+	sudo pkill -SIGINT zerotier-one || true
 	sleep 5
-	sudo pkill zerotier-one
+	echo kill
+	sudo pkill zerotier-one || true
 	sleep 5
+	echo copy ids
 	sudo cat /var/lib/zerotier-one/identity.public > IDS/$i.pub
 	sudo cat /var/lib/zerotier-one/identity.secret > IDS/$i.prv
+	echo remove old ids
 	sudo rm /var/lib/zerotier-one/identity.public
 	sudo rm /var/lib/zerotier-one/identity.secret
 done
+
+echo DONE
