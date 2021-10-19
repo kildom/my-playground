7493f078e5048a98fe8bdc5b729c4c6873ba9c12
kildom <kildom@users.noreply.github.com>
2021-10-19 18:06:47 +0200

    Create ids_gen.sh

---
+++ ids_gen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p IDS
+
+for i in {0..19}; do 
+    if [ -e IDS/$i.prv ]; then
+		continue
+	fi
+	sudo zerotier/zerotier-one -d
+	sleep 10
+	sudo pkill -SIGINT zerotier-one
+	sleep 5
+	sudo pkill zerotier-one
+	sleep 5
+	sudo cat /var/lib/zerotier-one/identity.public > IDS/$i.pub
+	sudo cat /var/lib/zerotier-one/identity.secret > IDS/$i.prv
+	sudo rm /var/lib/zerotier-one/identity.public
+	sudo rm /var/lib/zerotier-one/identity.secret
+done
